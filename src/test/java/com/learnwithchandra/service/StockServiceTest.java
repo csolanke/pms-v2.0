8ce2b492e7de2ba9475a727e595a21334ea160e4
Chandrakant <chandrakant@ChandrakantsMBP.lan>
2024-03-21 18:27:18 -0400

    Added dependency for wire-mock

--- src/test/java/com/learnwithchandra/service/StockServiceTest.java
+++ src/test/java/com/learnwithchandra/service/StockServiceTest.java
@@ -5,18 +5,19 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.learnwithchandra.entities.Stock;
 import com.learnwithchandra.repository.StockRepository;
 
 @ExtendWith(MockitoExtension.class)
-public class StockServiceTest {
+class StockServiceTest {
 
     @Mock
     private StockRepository stockRepositoryMock;
@@ -24,19 +25,11 @@ public class StockServiceTest {
     @InjectMocks
     private StockService stockService;
 
-//    @Rule
-//    public WireMockRule wireMockRule = new WireMockRule(8080); // Assuming WireMock is running on port 8080
-//
-//    @BeforeEach
-//    public void setUp() {
-//        // Stubbing external API response using WireMock
-//        stubFor(post(urlEqualTo("/stocks"))
-//                .willReturn(aResponse()
-//                .withStatus(200)));
-//    }
+    @Rule
+   public WireMockRule wireMockRule = new WireMockRule(8080); // Assuming WireMock is running on port 808
 
     @Test
-    public void testAddStock() {
+    void testAddStock() {
         // Create a mock Stock object
         Stock stock = new Stock(/* constructor arguments */);
 
